68a0e4da8d846e5c18b854287bc4852bebc7a89a
su9257 <su9257@outlook.com>
2019-09-03 22:23:08 +0800

    更新

--- Assets/Scripts/Attribute/2.TypeSpecifics/AssetSelectorAttributeExample.cs
+++ Assets/Scripts/Attribute/2.TypeSpecifics/AssetSelectorAttributeExample.cs
@@ -1,22 +1,47 @@
 ﻿using UnityEngine;
 
-
-
-namespace D_Game.Demo
+using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+public class AssetSelectorAttributeExample : MonoBehaviour
 {
-    public class AssetSelectorAttributeExample : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-            
-        }
-    
-        // Update is called once per frame
-        void Update()
-        {
-            
-        }
-    }
+    [AssetSelector]
+    public Material AnyAllMaterials;
+
+    [AssetSelector]
+    public Material[] ListOfAllMaterials;
+
+    [AssetSelector(FlattenTreeView = true)]//非默认树状显示
+    public PhysicMaterial NoTreeView;
+
+    [AssetSelector(Paths = "Assets/MyScriptableObjects")]
+    public ScriptableObject ScriptableObjectsFromFolder;
+
+    [AssetSelector(Paths = "Assets/MyScriptableObjects|Assets/Other/MyScriptableObjects")]
+    public Material ScriptableObjectsFromMultipleFolders;
+
+    [AssetSelector(Filter = "New Animation t:AnimationClip l:自定义标签")]
+    public UnityEngine.Object AssetDatabaseSearchFilters;
+
+    [Title("Other Minor Features")]
+
+    [AssetSelector(DisableListAddButtonBehaviour = true)] //开启后不会出现树形下拉条
+    public List<GameObject> DisableListAddButtonBehaviour;
+
+    [AssetSelector(DrawDropdownForListElements = false)]
+    public List<GameObject> DisableListElementBehaviour;
+
+    [AssetSelector(ExcludeExistingValuesInList = false)]
+    public List<GameObject> ExcludeExistingValuesInList;
+
+    [AssetSelector(IsUniqueList = false)]
+    public List<GameObject> DisableUniqueListBehaviour;
+
+    [AssetSelector(ExpandAllMenuItems = true)]
+    public List<GameObject> ExpandAllMenuItems;
+
+    [AssetSelector(DropdownTitle = "Custom Dropdown Title")]
+    public List<GameObject> CustomDropdownTitle;
+
 }
 
---
+++ Assets/Scripts/Attribute/2.TypeSpecifics/ColorPaletteAttributeExample.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace D_Game.Demo
+{
+    public class ColorPaletteAttributeExample : MonoBehaviour
+    {
+        [ColorPalette]
+        public Color ColorOptions;
+
+        [ColorPalette("Underwater")]
+        public Color UnderwaterColor;
+
+        [ColorPalette("My Palette")]
+        public Color MyColor;
+
+        public string DynamicPaletteName = "Clovers";
+
+        // The ColorPalette attribute supports both 
+        // member references and attribute expressions.
+        [ColorPalette("$DynamicPaletteName")]
+        public Color DynamicPaletteColor;
+
+        [ColorPalette("Fall"), HideLabel]
+        public Color WideColorPalette;
+
+        [ColorPalette("菜鸟海澜调色板")]
+        public Color[] ColorArray;
+
+        // ------------------------------------
+        // Color palettes can be accessed and modified from code.
+        // Note that the color palettes will NOT automatically be included in your builds.
+        // But you can easily fetch all color palettes via the ColorPaletteManager 
+        // and include them in your game like so:
+        // ------------------------------------
+
+        [FoldoutGroup("Color Palettes", expanded: false)]
+        [ListDrawerSettings(IsReadOnly = true)]
+        [PropertyOrder(9)]
+        public List<ColorPalette> ColorPalettes;
+
+#if UNITY_EDITOR
+
+        [FoldoutGroup("Color Palettes"), Button(ButtonSizes.Large), GUIColor(0, 1, 0), PropertyOrder(8)]
+        private void FetchColorPalettes()
+        {
+            this.ColorPalettes = Sirenix.OdinInspector.Editor.ColorPaletteManager.Instance.ColorPalettes
+                .Select(x => new ColorPalette()
+                {
+                    Name = x.Name,
+                    Colors = x.Colors.ToArray()
+                })
+                .ToList();
+        }
+
+#endif
+        [Serializable]
+        public class ColorPalette
+        {
+            [HideInInspector]
+            public string Name ;
+
+            [LabelText("$Name")]
+            [ListDrawerSettings(IsReadOnly = true, Expanded = false)]//IsReadOnly是否可以删除序列化数组  Expanded覆盖默认设置，是否展开，false为不展开状态
+            public Color[] Colors;
+        }
+    }
+}
+
