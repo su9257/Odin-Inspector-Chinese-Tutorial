b0b52f126ba64c3d04bad06b72eb6fb985b990e6
su9257 <su9257@outlook.com>
2019-09-08 02:35:03 +0800

    添加示例资源、文件夹整理

---
+++ Assets/Scripts/Odin-Inspector-Chinese-Tutorial/2.Attribute/1.Essentials/AssetsOnlyExample.cs
@@ -0,0 +1,28 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetsOnlyExample : MonoBehaviour
+{
+    [AssetsOnly]
+    public List<GameObject> assetsOnlyPrefabList;
+
+    [AssetsOnly]
+    public GameObject ssetsOnlyPrefab;
+
+    [AssetsOnly]
+    public Material assetsOnlyMaterial;
+
+    [AssetsOnly]
+    public MeshRenderer someMeshRendererOnPrefab;
+
+    [SceneObjectsOnly]
+    public List<GameObject> onlySceneObjectList;
+
+    [SceneObjectsOnly]
+    public GameObject someSceneObject;
+
+    [SceneObjectsOnly]
+    public MeshRenderer someMeshRendererInScene;
+}
