824410c23b4f949bc341fb06da38a5f176967e17
su9257 <su9257@outlook.com>
2019-09-22 22:59:24 +0800

    更新

---
+++ Assets/Scripts/Odin-Inspector-Chinese-Tutorial/2.Attribute/3.Groups/InlineButtonAttributeExample.cs
@@ -0,0 +1,25 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InlineButtonAttributeExample : MonoBehaviour
+{
+    // Inline Buttons:
+    [InlineButton("A")]
+    public int InlineButton;
+
+    [InlineButton("A")]
+    [InlineButton("B", "Custom Button Name")]
+    public int ChainedButtons;
+
+    private void A()
+    {
+        Debug.Log("A");
+    }
+
+    private void B()
+    {
+        Debug.Log("B");
+    }
+}
