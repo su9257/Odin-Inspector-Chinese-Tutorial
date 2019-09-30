a6b289acd559a8f4c21ecaba644890c99f518fe4
su9257 <su9257@outlook.com>
2019-09-30 11:56:18 +0800

    更新

---
+++ Assets/Scripts/Odin-Inspector-Chinese-Tutorial/2.Attribute/10.Meta/SuppressInvalidAttributeErrorExample.cs
@@ -0,0 +1,13 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuppressInvalidAttributeErrorExample : MonoBehaviour
+{
+    [Range(0, 10)]
+    public string InvalidAttributeError = "This field will have an error box for the Range attribute on a string field.";
+
+    [Range(0, 10), SuppressInvalidAttributeError]
+    public string SuppressedError = "The error has been suppressed on this field, and thus no error box will appear.";
+}
