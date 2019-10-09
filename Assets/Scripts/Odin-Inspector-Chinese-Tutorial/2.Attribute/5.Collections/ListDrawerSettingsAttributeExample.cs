c6797855de39bc30a5c987a9042952dcb7358c2d
su9257 <su9257@outlook.com>
2019-10-10 00:47:29 +0800

    Collections

--- Assets/Scripts/Odin-Inspector-Chinese-Tutorial/2.Attribute/5.Collections/ListDrawerSettingsAttributeExample.cs
+++ Assets/Scripts/Odin-Inspector-Chinese-Tutorial/2.Attribute/5.Collections/ListDrawerSettingsAttributeExample.cs
@@ -17,13 +17,13 @@ public class ListDrawerSettingsAttributeExample : MonoBehaviour
     [InfoBox("现在可以拖动列表元素来重新排序并逐个删除它们，并且列表具有分页功能(尝试添加大量元素!)您仍然可以从项目视图一次将许多资产拖到列表中—只需将它们拖到列表本身，并将它们插入到您想要添加它们的地方.")]
     public List<float> FloatList;
 
+
     [InfoBox("将[Range]属性应用于此列表，代替传统的float形式")]
     [Range(0, 1)]
     public float[] FloatRangeArray;
 
     [ListDrawerSettings(IsReadOnly = true)]
     public int[] ReadOnlyArray1 = new int[] { 1, 2, 3 };
-
     [ReadOnly]
     public int[] ReadOnlyArray2 = new int[] { 1, 2, 3 };
 
@@ -42,23 +42,18 @@ public class ListDrawerSettingsAttributeExample : MonoBehaviour
 
     [ListDrawerSettings(OnBeginListElementGUI = "BeginDrawListElement", OnEndListElementGUI = "EndDrawListElement")]
     public SomeStruct[] InjectListElementGUI;
-
-    [ListDrawerSettings(OnTitleBarGUI = "DrawRefreshButton")]
-    public List<int> CustomButtons;
-
-    [ListDrawerSettings(CustomAddFunction = "CustomAddFunction")]
-    public List<int> CustomAddBehaviour;
-
     private void BeginDrawListElement(int index)
     {
         SirenixEditorGUI.BeginBox(this.InjectListElementGUI[index].SomeString);
     }
-
     private void EndDrawListElement(int index)
     {
         SirenixEditorGUI.EndBox();
     }
 
+
+    [ListDrawerSettings(OnTitleBarGUI = "DrawRefreshButton")]
+    public List<int> CustomButtons;
     private void DrawRefreshButton()
     {
         if (SirenixEditorGUI.ToolbarButton(EditorIcons.Refresh))
@@ -67,6 +62,8 @@ public class ListDrawerSettingsAttributeExample : MonoBehaviour
         }
     }
 
+    [ListDrawerSettings(CustomAddFunction = "CustomAddFunction")]
+    public List<int> CustomAddBehaviour;
     private int CustomAddFunction()
     {
         return this.CustomAddBehaviour.Count+100;
