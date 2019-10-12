c65bbd61718a55f3fa7ea6b51906b04e1329309e
su9257 <su9257@outlook.com>
2019-10-12 11:51:28 +0800

    更新conditionals

--- Assets/Scripts/Odin-Inspector-Chinese-Tutorial/2.Attribute/6.Conditionals/DisableInPrefabInstancesAttributeExample.cs
+++ Assets/Scripts/Odin-Inspector-Chinese-Tutorial/2.Attribute/6.Conditionals/DisableInPrefabInstancesAttributeExample.cs
@@ -1,6 +1,4 @@
 ﻿using Sirenix.OdinInspector;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DisableInPrefabInstancesAttributeExample : MonoBehaviour
