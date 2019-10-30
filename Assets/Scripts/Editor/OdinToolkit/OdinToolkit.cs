0aa73239324690d804590ed6cdb4b4f9ba0f2163
su9257 <su9257@outlook.com>
2019-10-30 20:04:14 +0800

    搜索重复文件0.1

---
+++ Assets/Scripts/Editor/OdinToolkit/OdinToolkit.cs
@@ -0,0 +1,61 @@
+﻿using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using UnityEngine;
+using System;
+using Sirenix.Utilities.Editor;
+using Sirenix.Utilities;
+
+public class OdinToolkit : OdinMenuEditorWindow
+{
+    [MenuItem("Tools/Odin Toolkit")]
+    private static void OpenWindow()
+    {
+        var window = GetWindow<OdinToolkit>();
+        window.position = GUIHelper.GetEditorWindowRect().AlignCenter(1000, 500);
+    }
+
+    protected override OdinMenuTree BuildMenuTree()
+    {
+        var tree = new OdinMenuTree(false, OdinMenuStyle.TreeViewStyle);
+        tree.Config.DrawSearchToolbar = true;
+        tree.DefaultMenuStyle.Height = 50;
+        tree.DefaultMenuStyle.IconSize = 40;
+
+        this.titleContent = new GUIContent("Odin 工具箱");
+
+        tree.Add("菜单风格设置", tree.DefaultMenuStyle,EditorIcons.House);
+        tree.Add("一键搜索重复文件", new OneKeySearchDuplicateFiles(),EditorIcons.OdinInspectorLogo) ;
+        tree.Add("SomeType", new SomeType());
+        tree.Add("MyTargetEditorWindow", new MyTargetEditorWindow());
+        tree.Add("Settings", GeneralDrawerConfig.Instance);
+        tree.Add("Utilities", new TextureUtilityEditor());
+        tree.Add("MyHybridEditorWindowOne", new MyHybridEditorWindowOne());
+        //tree.EnumerateTree().AddThumbnailIcons(); 
+        return tree;
+    }
+
+    public OdinMenuStyle ReturnOdinMenuStyle()
+    {
+        OdinMenuStyle odinMenuStyle =    new OdinMenuStyle()
+        {
+            Height = 50,
+            Offset = 20.00f,
+            IndentAmount = 15.00f,
+            IconSize = 35,
+            IconOffset = 0.00f,
+            NotSelectedIconAlpha = 0.85f,
+            IconPadding = 0.00f,
+            TriangleSize = 16.00f,
+            TrianglePadding = 0.00f,
+            AlignTriangleLeft = true,
+            Borders = true,
+            BorderPadding = 0.00f,
+            BorderAlpha = 0.32f,
+            SelectedColorDarkSkin = new Color(0.243f, 0.373f, 0.588f, 1.000f),
+            SelectedColorLightSkin = new Color(0.243f, 0.490f, 0.900f, 1.000f)
+        };
+        return odinMenuStyle;
+    }
+}
+
+
---
+++ Assets/Scripts/Editor/OdinToolkit/OneKeySearchDuplicateFiles.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Timeline;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using System.Threading;
+
+public class OneKeySearchDuplicateFiles : SerializedScriptableObject
+{
+    private bool IsToggled;
+    private int maxCount;
+    private IEnumerator<FileInfo> fileInfoIEnumerator;
+
+    [PropertySpace(10)]
+    [Title("需要搜索的文件夹", "默认为Asset全目录", titleAlignment: TitleAlignments.Split)]
+    [FolderPath(ParentFolder = "Assets", RequireExistingPath = true, AbsolutePath = true)]
+    [LabelText("选择你要搜索的文件夹")]
+    public string targetSearchFolder;
+
+    [ShowInInspector]
+    private Dictionary<string, List<string>> objectGroup = new Dictionary<string, List<string>>();
+
+    [ShowIf("@ IsToggled== false")]
+    [Button("开始搜索", ButtonSizes.Large)]
+    public void StartSearch()
+    {
+        if (string.IsNullOrEmpty(targetSearchFolder))
+        {
+            targetSearchFolder = Application.dataPath;
+            Debug.Log(targetSearchFolder);
+        }
+        ResetData();
+        DirectoryInfo directoryInfo = new DirectoryInfo(targetSearchFolder);
+        var filesGroup = directoryInfo.EnumerateFiles("*", SearchOption.AllDirectories).Where(x => x.Extension != ".meta");
+
+        maxCount = filesGroup.Count();
+        fileInfoIEnumerator = filesGroup.GetEnumerator();
+        IsToggled = true;
+        EditorApplication.update += Updte;
+    }
+    private void ResetData()
+    {
+        maxCount = 0;
+        MaxCount = 0;
+        fileInfoIEnumerator = null;
+    }
+
+    private void FilterDictionary()
+    {
+        objectGroup = objectGroup.Where(x => x.Value.Count > 1).ToDictionary(p=>p.Key, p => p.Value);
+    }
+
+    [ReadOnly]
+    [ProgressBar(0, "maxCount", DrawValueLabel = true, ValueLabelAlignment = TextAlignment.Left, ColorMember = "GetHealthBarColor", Height = 30)]
+    [ShowInInspector]
+    [HideLabel]
+    [ShowIf("@ IsToggled== true")]
+    public int MaxCount { get; set; }
+
+    private Color GetHealthBarColor(int value)
+    {
+        maxCount = maxCount == 0 ? 1 : maxCount;
+        return Color.Lerp(Color.red, Color.green, Mathf.Pow((float)value / maxCount, 2));
+    }
+    public void Updte()
+    {
+        if (IsToggled)
+        {
+            if (fileInfoIEnumerator.MoveNext())
+            {
+                string hashValue = GetMD5HashFromFile(fileInfoIEnumerator.Current.FullName);
+                if (!objectGroup.ContainsKey(hashValue))
+                {
+                    objectGroup[hashValue] = new List<string>();
+                }
+                objectGroup[hashValue].Add("名称为：" + fileInfoIEnumerator.Current.Name);
+                Debug.Log(hashValue);
+                ++MaxCount;
+            }
+            else
+            {
+                EditorApplication.update -= Updte;
+                IsToggled = false;
+                FilterDictionary();
+                Debug.Log("注销");
+            }
+        }
+    }
+
+    //[PropertyOrder(10000)]
+    //[TextArea(10, 30)]
+    //[LabelText("打印内容")]
+    //public string LogContent;
+
+    public string GetMD5HashFromFile(string fileFullName)
+    {
+        try
+        {
+            FileStream file = new FileStream(fileFullName, FileMode.Open);
+            System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] retVal = md5.ComputeHash(file);
+            file.Close();
+            return BitConverter.ToString(retVal).ToLower().Replace("-", "");
+        }
+        catch
+        {
+            throw;
+        }
+
+    }
+
+    public void TestGetFilePaths(string absolutePath)
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(absolutePath);
+        //遍历文件夹
+        foreach (DirectoryInfo tempDirectoryInfo in directoryInfo.GetDirectories())
+        {
+            TestGetFilePaths(tempDirectoryInfo.FullName);
+        }
+        foreach (FileInfo fileInfo in directoryInfo.GetFiles())
+        {
+            Debug.Log(fileInfo.FullName);
+        }
+    }
+
+    //GetFilePaths(before + "/UI", dic, ".png");
+    private static void GetFilePaths(string absolutePath, Dictionary<string, string> _dic, string extension)
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(absolutePath);
+        //遍历文件夹
+        foreach (DirectoryInfo tempDirectoryInfo in directoryInfo.GetDirectories())
+        {
+            GetFilePaths(tempDirectoryInfo.FullName, _dic, extension);
+        }
+
+        //遍历文件
+        foreach (FileInfo fileInfo in directoryInfo.GetFiles())
+        {
+            if (fileInfo.Extension.ToLower().Equals(extension.ToLower()))
+            {
+                try
+                {
+                    string tempStr = fileInfo.FullName.Substring(fileInfo.FullName.LastIndexOf("\\Resources\\") + 11);
+                    string value = tempStr.Substring(0, tempStr.Length - extension.Length);
+                    _dic.Add(fileInfo.Name, value);
+                }
+                catch
+                {
+                    Debug.Log("<color=#FF4040>发现同名资源: </color>" + fileInfo.Name + " " + fileInfo.FullName.Substring(Application.dataPath.Length - "Asset".Length - 1));
+                    Debug.Log("<color=#FF4040>已存在同名资源: </color>" + fileInfo.Name + " " + _dic[fileInfo.Name] + extension);
+                }
+            }
+        }
+    }
+}
