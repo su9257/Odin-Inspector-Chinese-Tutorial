a568ba28c8fc271c698af0b11de8505114c1f4b2
su9257 <su9257@outlook.com>
2019-11-20 20:14:26 +0800

    准备验证器相关

---
+++ Assets/Scripts/7.Validator/TestValidatorComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Timeline;
+using TMPro;
+using Sirenix.OdinInspector;
+
+public class TestValidatorComponent : MonoBehaviour
+{
+    [Required("需要一个Obj", MessageType = InfoMessageType.Warning)]
+    public GameObject tempObj;
+    void Start()
+    {
+
+    }
+
+
+}
+
+
