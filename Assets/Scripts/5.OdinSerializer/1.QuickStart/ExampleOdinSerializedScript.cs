1d45e6b1b35f7f05980697cc4016ee5497c76e2f
su9257 <su9257@outlook.com>
2019-12-02 19:26:45 +0800

    初识Odin序列化

--- Assets/Scripts/5.OdinSerializer/1.QuickStart/ExampleOdinSerializedScript.cs
+++ Assets/Scripts/5.OdinSerializer/1.QuickStart/ExampleOdinSerializedScript.cs
@@ -7,37 +7,37 @@ using UnityEngine;
 
 public class ExampleOdinSerializedScript : SerializedMonoBehaviour
 {
-    // Unity will not serialize. Serialized by Odin.
-    public Dictionary<int, string> FirstDictionary= new Dictionary<int, string>();
+    // 使用Odin序列化，而非Unity序列化
+    public Dictionary<int, string> firstDictionary= new Dictionary<int, string>();
 
-    // Unity will serialize. NOT serialized by Odin.
-    public MyClassByUnity MyUnityReference = new MyClassByUnity();
+    // MyClassByUnity 因为标记为 Serializable ,所以使用Unity 自带的序列化，而非Odin 序列化
+    public MyClassByUnity myUnityReference = new MyClassByUnity();
 
+    //强制使用 Odin 序列化，而不使用Unity的序列化
     [NonSerialized, OdinSerialize]
-    public MyClassByOdin MyOdinReference = new MyClassByOdin();
+    public MyClassByOdin myOdinReference = new MyClassByOdin();
 
     private void Start()
     {
-        Debug.Log(FirstDictionary.Count);
-        Debug.Log(MyUnityReference.SecondDictionary.Count);
-        Debug.Log(MyOdinReference.SecondDictionary.Count);
+        Debug.Log(firstDictionary.Count);
+        Debug.Log(myUnityReference.secondDictionary_Unity.Count);
+        Debug.Log(myOdinReference.secondDictionary_Odin.Count);
     }
+    
 }
 
 [Serializable]
 public class MyClassByUnity
 {
-    // Despite the OdinSerialize attribute, this field is not serialized.
+    // 虽然标记为 OdinSerialize 特性, 但是依然不会被序列化
     [OdinSerialize]
-    public Dictionary<int, string> SecondDictionary = new Dictionary<int, string>();
+    public Dictionary<int, string> secondDictionary_Unity = new Dictionary<int, string>();
 }
 
 [Serializable]
 public class MyClassByOdin
 {
-    // Despite the OdinSerialize attribute, this field is not serialized.
     [OdinSerialize]
-    public Dictionary<int, string> SecondDictionary= new Dictionary<int, string>();
+    [NonSerialized]
+    public Dictionary<int, string> secondDictionary_Odin= new Dictionary<int, string>();
 }
-
-
---
+++ Assets/Scripts/5.OdinSerializer/1.QuickStart/ExampleUnitySerializedScript.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+
+public class ExampleUnitySerializedScript : MonoBehaviour
+{
+    public Dictionary<int, string> keyValuePairs_0 = new Dictionary<int, string>();
+
+    [ShowInInspector]
+    public Dictionary<int, string> keyValuePairs_1 = new Dictionary<int, string>();
+}
+
+
