451ceb52073ae66a6eb89895effa5da21b206064
su9257 <su9257@outlook.com>
2019-12-06 22:15:16 +0800

    序列化相关

---
+++ Assets/Scripts/5.OdinSerializer/2.Implementing The Odin Serializer/CustomSerializedMonoBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using System.Collections.Generic;
+using UnityEngine;
+[ShowOdinSerializedPropertiesInInspector]
+public class CustomSerializedMonoBehaviour : MonoBehaviour, ISerializationCallbackReceiver, ISupportsPrefabSerialization
+{
+    [SerializeField, HideInInspector]
+    private SerializationData serializationData;
+
+    SerializationData ISupportsPrefabSerialization.SerializationData { get { return this.serializationData; } set { this.serializationData = value; } }
+
+    void ISerializationCallbackReceiver.OnAfterDeserialize()
+    {
+        UnitySerializationUtility.DeserializeUnityObject(this, ref this.serializationData);
+    }
+
+    void ISerializationCallbackReceiver.OnBeforeSerialize()
+    {
+        UnitySerializationUtility.SerializeUnityObject(this, ref this.serializationData);
+    }
+
+    public Dictionary<string, string> keyValuePairs = new Dictionary<string, string>(); 
+}
+
+
---
+++ Assets/Scripts/5.OdinSerializer/2.Implementing The Odin Serializer/CustomSerializedScriptableObject.cs
@@ -0,0 +1,23 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+
+
+[ShowOdinSerializedPropertiesInInspector]
+public class CustomSerializedScriptableObject : ScriptableObject, ISerializationCallbackReceiver
+{
+    [SerializeField, HideInInspector]
+    private SerializationData serializationData;
+
+    void ISerializationCallbackReceiver.OnAfterDeserialize()
+    {
+        UnitySerializationUtility.DeserializeUnityObject(this, ref this.serializationData);
+    }
+
+    void ISerializationCallbackReceiver.OnBeforeSerialize()
+    {
+        UnitySerializationUtility.SerializeUnityObject(this, ref this.serializationData);
+    }
+}
+
+
