84893351c21943fdec7623affff51d767cc31793
su9257 <su9257@outlook.com>
2020-02-06 01:09:59 +0800

    使用Odin-Serializer保存游戏数据

---
+++ Assets/Scripts/5.OdinSerializer/3.Making Save Games With The Odin Serializer/OdinPlayerPrefs.cs
@@ -0,0 +1,173 @@
+﻿using Sirenix.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public sealed class OdinPlayerPrefs
+{
+
+    #region Singleton
+    public const string Name = "OdinPlayerPrefs";
+    static OdinPlayerPrefs()
+    {
+        absoluteDirectoryPath = Path.Combine(Application.persistentDataPath, "User");
+        if (!Directory.Exists(absoluteDirectoryPath))
+        {
+            Directory.CreateDirectory(absoluteDirectoryPath);
+        }
+        fileFullName = Path.Combine(absoluteDirectoryPath, fileName);
+        LoadData();
+    }
+    public OdinPlayerPrefs() { }
+
+    #endregion
+
+    public static string absoluteDirectoryPath;
+    public const string fileName = "UserConfig";
+    public static string fileFullName;
+
+    public static DataFormat dataFormat = DataFormat.Binary;
+    private static UserInfo userInfo;
+
+    private const string defaultString = "";
+    private const float defaultFloat = 0;
+    private const int defaultInt = 0;
+
+    [Serializable]
+    public class UserInfo
+    {
+        public Dictionary<string, string> keyValuePairs_String = new Dictionary<string, string>();
+        public Dictionary<string, float> keyValuePairs_Float = new Dictionary<string, float>();
+        public Dictionary<string, int> keyValuePairs_Int = new Dictionary<string, int>();
+    }
+
+
+    private static void SaveData()
+    {
+        byte[] bytes = SerializationUtility.SerializeValue(userInfo, dataFormat);
+        File.WriteAllBytes(fileFullName, bytes);
+    }
+    private static void LoadData()
+    {
+        if (!File.Exists(fileFullName))
+        {
+            userInfo = new UserInfo();
+            return;
+        }
+        byte[] bytes = File.ReadAllBytes(fileFullName);
+        userInfo = SerializationUtility.DeserializeValue<UserInfo>(bytes, dataFormat);
+    }
+
+
+    public static void DeleteAll()
+    {
+        userInfo.keyValuePairs_String.Clear();
+        userInfo.keyValuePairs_Float.Clear();
+        userInfo.keyValuePairs_Int.Clear();
+        SaveData();
+    }
+    public static void DeleteKey(string key)
+    {
+        bool isNeedSaveData = false;
+        if (userInfo.keyValuePairs_String.ContainsKey(key))
+        {
+            userInfo.keyValuePairs_String.Remove(key);
+            isNeedSaveData = true;
+        }
+        if (userInfo.keyValuePairs_Float.ContainsKey(key))
+        {
+            userInfo.keyValuePairs_Float.Remove(key);
+            isNeedSaveData = true;
+        }
+        if (userInfo.keyValuePairs_Int.ContainsKey(key))
+        {
+            userInfo.keyValuePairs_Int.Remove(key);
+            isNeedSaveData = true;
+        }
+        if (isNeedSaveData)
+        {
+            SaveData();
+        }
+        else
+        {
+            Debug.LogWarning($"删除失败，没有找到指定Key:{key}");
+        }
+    }
+    public static float GetFloat(string key)
+    {
+        return GetFloat(key, defaultFloat);
+    }
+    public static float GetFloat(string key, float defaultValue)
+    {
+        if (userInfo.keyValuePairs_Float.ContainsKey(key))
+        {
+            return userInfo.keyValuePairs_Float[key];
+        }
+        else
+        {
+            return defaultValue;
+        }
+    }
+    public static int GetInt(string key)
+    {
+        return GetInt(key, defaultInt);
+    }
+    public static int GetInt(string key, int defaultValue)
+    {
+        if (userInfo.keyValuePairs_Int.ContainsKey(key))
+        {
+            return userInfo.keyValuePairs_Int[key];
+        }
+        else
+        {
+            return defaultValue;
+        }
+    }
+    public static string GetString(string key)
+    {
+        return GetString(key, defaultString);
+    }
+    public static string GetString(string key, string defaultValue)
+    {
+        if (userInfo.keyValuePairs_String.ContainsKey(key))
+        {
+            return userInfo.keyValuePairs_String[key];
+        }
+        else
+        {
+            return defaultValue;
+        }
+    }
+    public static bool HasKey(string key)
+    {
+        if (userInfo.keyValuePairs_String.ContainsKey(key))
+        {
+            return true;
+        }
+        if (userInfo.keyValuePairs_Float.ContainsKey(key))
+        {
+            return true;
+        }
+        if (userInfo.keyValuePairs_Int.ContainsKey(key))
+        {
+            return true;
+        }
+        return false;
+    }
+    public static void SetFloat(string key, float value)
+    {
+        userInfo.keyValuePairs_Float[key] = value;
+        SaveData();
+    }
+    public static void SetInt(string key, int value)
+    {
+        userInfo.keyValuePairs_Int[key] = value;
+        SaveData();
+    }
+    public static void SetString(string key, string value)
+    {
+        userInfo.keyValuePairs_String[key] = value;
+        SaveData();
+    }
+}
