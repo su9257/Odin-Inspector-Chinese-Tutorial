acfa816282555188f0c65852d3bd61061df5b611
su9257 <su9257@outlook.com>
2020-02-23 01:32:32 +0800

    添加static inspector示例先关

---
+++ Assets/Scripts/8.Odin static Inspector/StaticInspectorTutorials.cs
@@ -0,0 +1,45 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticInspectorTutorials : MonoBehaviour
+{
+    public enum TempEnum
+    {
+        One,Two,Three
+    }
+    public static TempEnum tempEnum;
+    public static string tempStr;
+    public static int tempInt;
+    public static List<StaticInspectorTutorials_One> staticInspectorTutorials_Ones = new List<StaticInspectorTutorials_One>();
+
+    [Button(ButtonSizes.Large)]
+    public static void TestStaticFunction()
+    {
+        Debug.Log("TestFunction");
+    }
+    [Button(ButtonSizes.Large, ButtonStyle.FoldoutButton)]
+    public static void TestStaticFunction(string str)
+    {
+        Debug.Log($"TestFunction:{str}");
+    }
+
+    [Button(ButtonSizes.Large, ButtonStyle.FoldoutButton)]
+    public static void TestStaticFunction(List<string> tempList)
+    {
+        for (int i = 0; i < tempList.Count; i++)
+        {
+            Debug.Log($"List Index :{i}---value:{tempList[i]}");
+        }
+    }
+
+    public void NoStaticFunction()
+    {
+        Debug.Log("NoStaticFunction");
+    }
+}
+
+public  class StaticInspectorTutorials_One
+{
+    public static string tempStr;
+}
